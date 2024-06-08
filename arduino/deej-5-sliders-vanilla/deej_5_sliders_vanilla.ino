a2fcd0fe2ea82f099ec90be1758431f261aa23ee
mohasali <78690099+mohasali@users.noreply.github.com>
2024-06-09 00:59:53 +0100

    Updated Arduino Code
    Added averaging, low-pass filtering, and median filtering for analog values. This is a better implementation than just waiting for a large change as now when changing volume you can change it smoothly ( 1%)

--- arduino/deej-5-sliders-vanilla/deej_5_sliders_vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej_5_sliders_vanilla.ino
@@ -1,8 +1,8 @@
 #include <BluetoothSerial.h>
 
-const int MIN_CHANGE = 35;
+const int MIN_CHANGE = 5;
 const int NUM_SLIDERS = 4;
-const int analogInputs[NUM_SLIDERS] = {25,33,32,35};
+const int analogInputs[NUM_SLIDERS] = {26, 33, 32, 35};
 
 const int NUM_BUTTONS = 1;
 const int buttonInputs[NUM_BUTTONS] = {15};
@@ -24,6 +24,17 @@ BluetoothSerial SerialBT;
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+#define NUM_SAMPLES 10 // Reduced number of samples for faster response
+#define MEDIAN_SAMPLES 5 // Reduced for faster response
+float alpha = 0.5; // Increased alpha for faster response
+
+int readings[NUM_SLIDERS][NUM_SAMPLES] = {0};
+int medianSamples[MEDIAN_SAMPLES];
+int readIndex[NUM_SLIDERS] = {0};
+int total[NUM_SLIDERS] = {0};
+int average[NUM_SLIDERS] = {0};
+float smoothedValue[NUM_SLIDERS] = {0};
+
 void setup() { 
   SerialBT.begin();
   Serial.begin(9600);
@@ -32,42 +43,70 @@ void setup() {
 }
 
 void loop() {
-  checkSerialConnection(Serial,CONNECTION_USB);
-  checkSerialConnection(SerialBT,CONNECTION_BT);
+  checkSerialConnection(Serial, CONNECTION_USB);
+  checkSerialConnection(SerialBT, CONNECTION_BT);
 
-  if (connection== CONNECTION_BT && !SerialBT.hasClient()) {
+  if (connection == CONNECTION_BT && !SerialBT.hasClient()) {
     connection = CONNECTION_NONE;
     return;
   }
 
-  sendValues("Slider",analogSliderValues,updateSliderValues());
-  sendValues("Button",sentButtonValues,  checkButtonStates());
-  //printSliderValues(); // For debug
+  sendValues("Slider", analogSliderValues, updateSliderValues());
+  sendValues("Button", sentButtonValues, checkButtonStates());
+  // printSliderValues(); // For debug
   delay(50);
 }
 
 bool updateSliderValues() {
   bool valueChange = false;
-  for (int i = 0; i < NUM_SLIDERS; i++) {
-    int newVal = analogRead(analogInputs[i]);
-    int oldVal = analogSliderValues[i];
-    // Checks if the new value is higher or lower than the MIN_CHANGE
-    if(newVal > oldVal+MIN_CHANGE || newVal < oldVal-MIN_CHANGE){
-      if(newVal+MIN_CHANGE>1023){
-        analogSliderValues[i] = 1023;
-      }
-      else if(newVal-MIN_CHANGE*2<0){
-        analogSliderValues[i] = 0;
+  for (int potIndex = 0; potIndex < NUM_SLIDERS; potIndex++) {
+    // Take a new reading
+    int newValue = analogRead(analogInputs[potIndex]);
 
-      }
-      else{
-        analogSliderValues[i] = newVal;
+    // Update the total for averaging
+    total[potIndex] -= readings[potIndex][readIndex[potIndex]];
+    readings[potIndex][readIndex[potIndex]] = newValue;
+    total[potIndex] += readings[potIndex][readIndex[potIndex]];
+    readIndex[potIndex] = (readIndex[potIndex] + 1) % NUM_SAMPLES;
 
-      }
+    // Calculate the average
+    average[potIndex] = total[potIndex] / NUM_SAMPLES;
+
+    // Apply the low-pass filter
+    smoothedValue[potIndex] = alpha * average[potIndex] + (1 - alpha) * smoothedValue[potIndex];
+
+    // Collect samples for median filtering
+    for (int i = 0; i < MEDIAN_SAMPLES; i++) {
+      medianSamples[i] = analogRead(analogInputs[potIndex]);
+      delay(2); // Small delay between samples
+    }
+
+    // Calculate the median
+    int medianValue = median(medianSamples, MEDIAN_SAMPLES);
+
+    // Combine the results
+    int combinedValue = (smoothedValue[potIndex] + medianValue) / 2;
+
+    if (combinedValue > analogSliderValues[potIndex] + MIN_CHANGE || combinedValue < analogSliderValues[potIndex] - MIN_CHANGE) {
+      analogSliderValues[potIndex] = combinedValue;
       valueChange = true;
     }
   }
   return valueChange;
+}
+
+int median(int *array, int size) {
+  int temp;
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = i + 1; j < size; j++) {
+      if (array[j] < array[i]) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array[size / 2];
 }
 
 void printSliderValues() {
@@ -83,16 +122,15 @@ void printSliderValues() {
   }
 }
 
-bool checkButtonStates(){
+bool checkButtonStates() {
   bool valueChange = false;
-  for(int i = 0; i < NUM_BUTTONS; i++){
+  for (int i = 0; i < NUM_BUTTONS; i++) {
     int newVal = digitalRead(buttonInputs[i]);
     int oldVal = buttonValues[i];
-    if(newVal == 1 && oldVal == 0){
+    if (newVal == 1 && oldVal == 0) {
       sentButtonValues[i] = 1;
       valueChange = true;
-    }
-    else{
+    } else {
       sentButtonValues[i] = 0;
     }
     buttonValues[i] = newVal;
@@ -100,9 +138,9 @@ bool checkButtonStates(){
   return valueChange;
 }
 
-void sendValues(String type,int* values, bool valueChange) {
-  if(valueChange){
-    String builtString = type+"|";
+void sendValues(String type, int* values, bool valueChange) {
+  if (valueChange) {
+    String builtString = type + "|";
 
     for (int i = 0; i < NUM_SLIDERS; i++) {
       builtString += String(values[i]);
@@ -111,7 +149,7 @@ void sendValues(String type,int* values, bool valueChange) {
         builtString += "|";
       }
     }
-    switch(connection){
+    switch (connection) {
       case CONNECTION_USB:
         Serial.println(builtString);
         break;
@@ -122,7 +160,7 @@ void sendValues(String type,int* values, bool valueChange) {
   }
 }
 
-void checkSerialConnection(Stream &serial,uint8_t connectionType) {
+void checkSerialConnection(Stream &serial, uint8_t connectionType) {
   if (serial.available()) {
     // Define a string to store the received data
     String data;
@@ -135,11 +173,10 @@ void checkSerialConnection(Stream &serial,uint8_t connectionType) {
       // Send a response if the data matches
       serial.println("yes");
       connection = connectionType;
-      // Set a dealy to ensure connection is properly established
+      // Set a delay to ensure connection is properly established
       delay(1000);
       updateSliderValues();
-      sendValues("Slider",analogSliderValues,true);
+      sendValues("Slider", analogSliderValues, true);
     }
   }
 }
-
