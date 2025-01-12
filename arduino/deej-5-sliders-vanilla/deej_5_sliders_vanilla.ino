a8e4265535334955b113992ed8e5d9bb46b733e1
mohasali <78690099+mohasali@users.noreply.github.com>
2025-01-12 15:28:27 +0000

    Update deej_5_sliders_vanilla.ino
    Added button double press feature.
    Fixed issue where button would not be detected

--- arduino/deej-5-sliders-vanilla/deej_5_sliders_vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej_5_sliders_vanilla.ino
@@ -2,32 +2,37 @@
 
 const int MIN_CHANGE = 20;
 const int NUM_SLIDERS = 4;
-const int analogInputs[NUM_SLIDERS] = {26, 33, 32, 35};
+const int analogInputs[NUM_SLIDERS] = {26,33, 32, 35};
 
-const int NUM_BUTTONS = 1;
-const int buttonInputs[NUM_BUTTONS] = {15};
+const int NUM_BUTTONS = 2;
+const int buttonInputs[NUM_BUTTONS] = {4, 16};
+
+const int DOUBLE_PRESS_THRESHOLD = 500; // Time threshold in milliseconds
 
 int analogSliderValues[NUM_SLIDERS];
 int buttonValues[NUM_BUTTONS];
-int sentButtonValues[NUM_BUTTONS];
+int sentButtonValues[NUM_BUTTONS * 2];
+unsigned long lastPressTime[NUM_BUTTONS]; // Timestamp of the last button press
+float buttonPressBuffer[NUM_BUTTONS]; // Timestamp of the last button press
 
 const uint8_t CONNECTION_NONE = 0;
 const uint8_t CONNECTION_USB = 1;
 const uint8_t CONNECTION_BT = 2;
 uint8_t connection = CONNECTION_NONE;
 
-bool isConnected = false;
 BluetoothSerial SerialBT;
 
-/* Check if Bluetooth configurations are enabled in the SDK */
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-void setup() { 
+void setup() {
   SerialBT.begin();
   Serial.begin(9600);
-  analogReadResolution(10);  // 10Bit resolution
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    pinMode(buttonInputs[i], INPUT_PULLDOWN);
+  }
+  analogReadResolution(10);
   delay(1000);
 }
 
@@ -40,86 +45,69 @@ void loop() {
     return;
   }
 
-  sendValues("Slider", analogSliderValues, updateSliderValues());
-  sendValues("Button", sentButtonValues, checkButtonStates());
-  // printSliderValues(); // For debug
+  sendValues("Slider", analogSliderValues, updateSliderValues(), NUM_SLIDERS);
+  sendValues("Button", sentButtonValues, checkButtonStates(), NUM_BUTTONS * 2);
   delay(50);
 }
+
 bool updateSliderValues() {
   bool valueChange = false;
   for (int potIndex = 0; potIndex < NUM_SLIDERS; potIndex++) {
-    // Take a new reading
     int newValue = analogRead(analogInputs[potIndex]);
-    
-    // Adjust threshold for detecting a change in the slider value
     if (abs(newValue - analogSliderValues[potIndex]) > MIN_CHANGE) {
-      // Clamp the values between 0 and 1023
-      if (newValue + MIN_CHANGE > 1023) {
-        analogSliderValues[potIndex] = 1023;
-      } else if (newValue - MIN_CHANGE < 0) {
-        analogSliderValues[potIndex] = 0;
-      } else {
-        analogSliderValues[potIndex] = newValue;
-      }
-
+      analogSliderValues[potIndex] = constrain(newValue, 0, 1023);
       valueChange = true;
     }
   }
   return valueChange;
-}
-
-
-int median(int *array, int size) {
-  int temp;
-  for (int i = 0; i < size - 1; i++) {
-    for (int j = i + 1; j < size; j++) {
-      if (array[j] < array[i]) {
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-    }
-  }
-  return array[size / 2];
-}
-
-void printSliderValues() {
-  for (int i = 0; i < NUM_SLIDERS; i++) {
-    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV");
-    Serial.write(printedString.c_str());
-
-    if (i < NUM_SLIDERS - 1) {
-      Serial.write(" | ");
-    } else {
-      Serial.write("\n");
-    }
-  }
 }
 
 bool checkButtonStates() {
   bool valueChange = false;
   for (int i = 0; i < NUM_BUTTONS; i++) {
     int newVal = digitalRead(buttonInputs[i]);
-    int oldVal = buttonValues[i];
-    if (newVal == 1 && oldVal == 0) {
+    unsigned long currentTime = millis();
+
+    // Check for single press
+    if (newVal == 1 && buttonValues[i] == 0) {
+      // Check for double press
+      if (currentTime - lastPressTime[i] <= DOUBLE_PRESS_THRESHOLD) {
+        sentButtonValues[NUM_BUTTONS + i] = 1;// Mark as double pressed
+        sentButtonValues[i] = 0; 
+        buttonPressBuffer[i] = DOUBLE_PRESS_THRESHOLD+1;
+        valueChange = true;
+      }
+      else{
+        buttonPressBuffer[i]=DOUBLE_PRESS_THRESHOLD;
+      }
+
+      lastPressTime[i] = currentTime; // Update last press time
+    }
+    else if(buttonPressBuffer[i]<=0){
+      buttonPressBuffer[i] = DOUBLE_PRESS_THRESHOLD+1;
       sentButtonValues[i] = 1;
       valueChange = true;
-    } else {
+      }
+    else if(buttonPressBuffer[i]<=DOUBLE_PRESS_THRESHOLD){
+      buttonPressBuffer[i]-=100;
+      }
+     else {
       sentButtonValues[i] = 0;
+      sentButtonValues[NUM_BUTTONS + i] = 0; // Reset double press state
     }
+
     buttonValues[i] = newVal;
   }
   return valueChange;
 }
 
-void sendValues(String type, int* values, bool valueChange) {
+void sendValues(String type, int* values, bool valueChange, int num) {
   if (valueChange) {
     String builtString = type + "|";
 
-    for (int i = 0; i < NUM_SLIDERS; i++) {
+    for (int i = 0; i < num; i++) {
       builtString += String(values[i]);
-
-      if (i < NUM_SLIDERS - 1) {
+      if (i < num - 1) {
         builtString += "|";
       }
     }
@@ -136,21 +124,14 @@ void sendValues(String type, int* values, bool valueChange) {
 
 void checkSerialConnection(Stream &serial, uint8_t connectionType) {
   if (serial.available()) {
-    // Define a string to store the received data
-    String data;
-    // Read and store the received data
-    data = serial.readString();
-    // Clean the data for comparison
+    String data = serial.readString();
     data.trim();
-    // Check if the received data matches
     if (data == "connect") {
-      // Send a response if the data matches
       serial.println("yes");
       connection = connectionType;
-      // Set a delay to ensure connection is properly established
       delay(1000);
       updateSliderValues();
-      sendValues("Slider", analogSliderValues, true);
+      sendValues("Slider", analogSliderValues, true, NUM_SLIDERS);
     }
   }
 }
